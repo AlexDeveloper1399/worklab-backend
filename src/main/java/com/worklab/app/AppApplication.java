1c8ae82e50fb9da8a9d530a20c9385d45ed529a9
Alexander <61645144+AlexDeveloper99@users.noreply.github.com>
2020-06-15 16:31:46 +0600

    first commit

---
+++ src/main/java/com/worklab/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.worklab.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
